3eb0c12b70d2b3dbf1e22119922241a4c0ff87a0
JasonAsare <mypaapa.asare@gmail.com>
2025-09-03 19:36:21 +0000

    commited

---
+++ logic.py
@@ -0,0 +1,18 @@
+import cv2
+
+
+def Camera():
+    cam = cv2.VideoCapture()
+
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcasade_frontalface_default.xml")
+
+    while True:
+        ret, frame = cam.read()
+        faces = face_cascade.detectMultiScale(frame, 1.1, 4)
+        for(x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y), (x+w,y+h),(0,255,0),4)
+        cv2.imshow("Face_Detection", frame)
+        if cv2.waitKey(1) &0xFF ==ord('q'):
+            break
+
+Camera()
